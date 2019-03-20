daa4871b3645e56b8fc5b2e82d0df8241710713b
Raman Yurkouski <ramanyurkouski@MacBook-Pro-Raman.local>
2019-03-20 22:49:43 +0300

    Second task

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/2/Diagonal.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/2/Diagonal.m
@@ -4,7 +4,18 @@
 
 // Complete the diagonalDifference function below.
 - (NSNumber *) diagonalDifference:(NSArray *)array {
-    return 0;
+    int sum1 = 0;
+    int sum2 = 0;
+    for (NSString *line in array) {
+        @autoreleasepool {
+        NSArray *lineArr = [line componentsSeparatedByString:@" " ];
+        sum1 += [[lineArr objectAtIndex:[array indexOfObject:line]] intValue];
+        sum2 += [[lineArr objectAtIndex:(lineArr.count - 1 - [array indexOfObject:line])] intValue];
+    }
+        
+    }
+    
+    return @(abs(sum1 - sum2));
 }
 
 @end
