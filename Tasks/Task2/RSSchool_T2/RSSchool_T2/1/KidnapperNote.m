997c62702d031c0a6318e7578aa24c1c899b002b
Elizaveta Gnezdilova <ramanyurkouski@MacBook-Pro-Raman.local>
2019-03-31 13:24:01 +0300

    First task

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
@@ -1,5 +1,15 @@
 #import "KidnapperNote.h"
 
 @implementation KidnapperNote
-// your code here
+-(BOOL)checkMagazine:(NSString *)magaine note:(NSString *)note{
+    NSArray *input_note = [[note lowercaseString] componentsSeparatedByString:@" "];
+    NSCountedSet *input_magazine = [[[ NSCountedSet alloc] initWithArray:[[magaine lowercaseString] componentsSeparatedByString:@" "] ] autorelease];
+    for (NSString *word in input_note) {
+        if (!([input_magazine countForObject:word]==0)) {
+            [input_magazine removeObject:word];
+        }
+        else{return NO;}
+    }
+    return YES;
+}
 @end
