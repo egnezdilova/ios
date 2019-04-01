450f1e4f342786d35d399c48818cbc6ff903b8c7
Elizaveta Gnezdilova <ramanyurkouski@MacBook-Pro-Raman.local>
2019-04-01 20:47:10 +0300

    Fifth task

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/5/TinyURL.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/5/TinyURL.m
@@ -1,5 +1,37 @@
 #import "TinyURL.h"
+@interface TinyURL ()
+    @property (nonatomic,retain,readonly)NSURL* baseURL;
+    @property (nonatomic,retain)NSMutableDictionary* urls;
+@end
 
 @implementation TinyURL
-// your code here
+
+@synthesize baseURL;
+@synthesize urls;
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        baseURL = [ [NSURL alloc] initWithString:@"https://egnezdilova/"];
+        urls = [[ NSMutableDictionary alloc] init];
+    }
+    return self;
+}
+- (void)dealloc
+{
+    [baseURL release];
+    [urls release];
+    [super dealloc];
+}
+- (NSURL *)encode:(NSURL *)originalURL{
+    NSString* newURL = [[[NSString alloc] initWithFormat:@"%d%lu",arc4random(),[urls count]] autorelease];
+    [urls setObject:originalURL forKey:newURL];
+    return [NSURL URLWithString:newURL relativeToURL:baseURL];
+}
+
+- (NSURL *)decode:(NSURL *)shortenedURL{
+    return urls[[shortenedURL.pathComponents objectAtIndex:1]];
+}
+
 @end
